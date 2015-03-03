0e896c6c18915cf9479644321b1119032eec8e89
Dylan <dook@stardust.red>
2015-03-02 18:09:23 -0800

    #9: Fixed syntax errors and various additions

--- date.cpp
+++ date.cpp
@@ -10,48 +10,54 @@ void  date::setYear(int  year) {  m_year =  year; }
 int date::getMonth() { return m_month; }
 int   date::getDay() {   return m_day; }
 int  date::getYear() {  return m_year; }
-string         str() {
+string date::str() {
   // Create temp string
   // Serialize month/day/year into string
   // return temp string
+  return "";
 }
 
 // Comparison operators
-bool operator==(const date& op) {
+bool date::operator==(const date& op) {
   // If years are equal
   //    If months are equal
   //      If days are equal
   //        return true
   // else return false
+  return false;
 }
-bool  operator<(const date& op) {
+bool  date::operator<(const date& op) {
   // If other's year is less than or equal to year
   //    If other's months is less than or equal to month
   //      If other's days is less than day
   //        return true
   // else return false
+  return false;
 }
 
-bool operator<=(const date& op) {
+bool date::operator<=(const date& op) {
   // If other's year is less than or equal to year
   //    If other's months is less than or equal to month
   //      If other's days is less than or equal to day
   //        return true
   // else return false
+  return false;
 }
 
-bool  operator>(const date& op) {
+bool  date::operator>(const date& op) {
   // If other's year is greater than or equal to year
   //    If other's months is greater than or equal to month
   //      If other's days is greater than day
   //        return true
   // else return false
+  return false;
 }
 
-bool operator>=(const date& op) {
+bool date::operator>=(const date& op) {
   // If other's year is greater than or equal to year
   //    If other's months is greater than or equal to month
   //      If other's days is greater than or equal to day
   //        return true
   // else return false
+  return false;
 }
--- db.cpp
+++ db.cpp
@@ -1,54 +1,119 @@
 #include "db.h"
 #include "book.h"
+using std::string;
 
 namespace DB {
-  Local::Local(string filename) {
-    // Set m_filename to filename
-  }
+  flag8 OPEN = 0x01;
+  flag8 FAIL = 0x02;
+  flag8 FEOF = 0x04;
+
+  Local::Local(string filename) { /* Initialize members*/ }
 
   bool Local::open(string filename) {
-    // Open db file for reading at beginning
-    // Save pointer to istream to m_file
-    // Set m_open to true
+    // if m_open is false
+    //    Open db file for reading at beginning
+    //    Save pointer to istream to m_file
+    //    set status flag open
+    //    Check for unused records
+    // else
+    //    Throw exception? or just ignore
+    //    Set status flag fail
+    // return false
+    return false;
   }
 
-  bool     Local::add(Book* book) {
+  int Local::add(Book* book) {
     // If an element of unused indices exist
     //    Assign first index to temp var
     //    Pop first element of unused indices list
     //    Assign index to book
-    //    return true
+    //    Write book with used flag
+    //    return index
     // else
     //    Set cursor to last record and save index + 1
     //    Set cursor to end record
     //    Write book with used flag
-    //    return true
+    //    return index
+    return 0;
   }
 
-  bool   Local::write(Book* book, bool used) {
-    // Write used
-    // Write book
-    // exception... return false
-    // return true
-  }
-
-  bool    Local::read(Book* book) {
+  int Local::read(Book* book) {
     // Move cursor up by one bool sizeof
     // Save record at cursor into book
+    // Set cursor up to next record
     // return true
+    return 0;
   }
 
-  bool  Local::remove(Book* book) {
-    // 
+  int Local::remove(Book* book) {
+    // Seek record at book's index
+    // Write unused
+    return 0;
   }
 
-  bool             Local::close() {
-    // Close db file
-    // Set m_open to false
+  bool Local::close() {
+    // if m_open is false
+    //    Throw exception? or ignore
+    // else
+    //    Close db file
+    //    Set m_open to false
+    return false;
   }
 
-  // Status methods
-  bool Local::eof()   { return false; }
+
+
+
+
+  // Internal methods
+  void Local::checkUnused() {
+    // If m_open is true:
+    //    Seek to beginning
+    //    While not end of file
+    //       Read next bool into temp var
+    //       If true, continue
+    //       else push into m_unusedIndex
+    // else
+    //    Throw exception and ignore
+  }
+
+  int Local::write(Book* book, bool used) {
+    // If m_open is true:
+    //    Write bool used
+    //    Write book structure
+    //    return book index
+    // else:
+    //    Throw exception
+    return 0;
+  }
+
+  void Local::seekb(int index) {
+    // Move cursor to index * align
+  }
+
+  void Local::seekr(int index) {
+    // Move cursor to alignment of current record
+    // Move cursor relative by index * align
+  }
+
+  void Local::seeke() {
+    // Move cursor to eof
+    // Move cursor to previous record
+  }
+
+  bool Local::clean() {
+    // Reindex database... low priority
+    return true;
+  }
+
+  // Status methods - low priority
+  bool Local::eof()   {
+    // if db file is at eof
+    //    clear file eof
+    //    return true
+    // else
+    //    return false
+    return false;
+  }
   bool Local::fail()  { return false; }
   bool Local::clear() { return false; }
 }
