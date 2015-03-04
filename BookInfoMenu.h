d854e1fee801dc62a297ff0b1a4186f41e6f4796
Alfred Ly <alfredly96@gmail.com>
2015-03-03 20:44:10 -0800

    #5: Menu Display planning
    initial commit

---
+++ BookInfoMenu.h
@@ -0,0 +1,27 @@
+#ifndef BOOKINFOMENU_H
+#define BOOKINFOMENU_H
+
+#include <string>
+#include <iomanip>
+#include "InventoryMenu.h"
+using namespace std;
+
+class BookInfoMenu 
+{ 
+	private:
+	public:
+		//Constructors
+//		BookInfoMenu();
+//		~BookInfoMenu();
+
+		void print()
+		{
+
+		}
+
+
+
+
+
+};
+#endif
---
+++ CashierMenu.h
@@ -0,0 +1,42 @@
+#ifndef CASHIERMENU_H
+#define CASHIERMENU_H
+
+
+#include <string>
+#include <iomanip>
+using namespace std;
+
+
+class CashierMenu 
+{
+	private:
+		int test;
+		int userCashMenu;
+	public:
+		//Constructors
+//		CashierMenu();
+//		~CashierMenu();
+
+		void print(int test)
+		{
+			cout << "Serendipity Booksellers Cashier Mode" << endl;
+			cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+			cout << "Date:\n"
+			 	 << "Qty" << "\tISBN" << "\t\tTitle" << "\t\t\tPrice"
+			  	 << "\t\tTotal" << endl;
+			cout << "________________________________________________________________\n\n" << endl;
+			cout << "\t\tSubtotal\n\n"
+				 << "\t\tTax\n\n"
+				 << "\t\tTotal" << endl;
+			cout << "Enter your Numerical Choice: " << endl;
+
+		}
+
+		void pushback(int userCashMenu)
+		{
+			// cout << "You have chosen option " << userCashMenu << endl;
+		
+		}
+};
+
+#endif
