da2ce82d3cd7d0130174d50e5fb4a411fae3780e
bobthewalrus <chantler.james@gmail.com>
2015-03-10 18:57:44 -0700

    #34: Flesh out cashier, functional

--- Cashier.cpp
+++ Cashier.cpp
@@ -1,1 +1,90 @@
+#include "Cashier.h" 
+#include <iostream>
+#include <vector>
+#include <string>
+#include "inventory.h"
+#include "book.h"
 
+Cashier::Cashier(Inventory* minv) {
+	inv = minv;
+}
+
+vector<Book*> Cashier::findISBN(string isbn) {
+	/*
+		Create temp string for isbn
+		Create temporary book list
+		Call inventory find book function with temp string and assign to temp book list
+		return temporary book list
+		*/
+	string userisbn = isbn;
+	vector<Book*> isbnsearch = inv->findBook(Book::ISBN, &userisbn);
+	return isbnsearch;
+}
+vector<Book*> Cashier::findAuthor(string author) {
+	/*
+		Create temp string for author
+		Create temporary book list
+		Call inventory find book function with temp string and assign to temp book list
+		return temporary book list
+	*/
+	string userauth = author;
+	vector<Book*> authsearch = inv->findBook(Book::AUTHOR, &userauth);
+	return authsearch;
+}
+vector<Book*> Cashier::findTitle(string title) {
+	/*
+	Create temp string for title
+	Create temporary book list
+	Call inventory find book function with temp string and assign to temp book list
+	return temporary book list
+	*/
+	string usertitle = title;
+	vector<Book*> titlesearch = inv->findBook(Book::TITLE, &usertitle);
+	return titlesearch;
+}
+
+bool Cashier::addCart(Book* book) {
+	/*
+		Add book to cart list
+	*/
+	m_cartlist.push_back(book);
+	return true;
+}
+
+bool Cashier::delCart(Book* book) {
+	/*
+		If book is in cart list
+			erase from cart list
+	*/
+	for (unsigned i = 0; i < m_cartlist.size(); i++)
+	{
+		if (book == m_cartlist[i]) {
+			// Erases element from m_cartlist
+			m_cartlist.erase( (m_cartlist.begin() + i) );
+		}
+	}
+	return true;
+}
+
+bool Cashier::clearCart() {
+	/*
+		Clear whole cart 
+	*/
+	m_cartlist.clear();
+	return true;
+}
+
+bool Cashier::purchaseCart() {
+	/*
+		For each item in cart list
+			delete book from inventory
+		Clear cart list
+	*/
+	for (unsigned i = 0; i < m_cartlist.size(); i++)
+	{
+		inv->delBook(m_cartlist[i]);
+
+	}
+	m_cartlist.clear();
+	return true;
+}
