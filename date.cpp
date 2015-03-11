5b7c4b4d391e3379c0ad04274a7ae98d3fe5b2af
Dylan <dook@stardust.red>
2015-03-10 22:49:04 -0700

    #49: Date functional

--- date.cpp
+++ date.cpp
@@ -1,6 +1,12 @@
 #include "date.h"
 using std::string;
 
+// Constructors
+date::date() : m_month(0), m_day(0), m_year(0) { }
+
+date::date(int mm, int dd, int yyyy) : // Initialize with MM-DD-YYYY format
+  m_month(mm), m_day(dd), m_year(yyyy) { }
+
 // Mutators
 void date::setMonth(int month) { m_month = month; }
 void   date::setDay(int   day) {   m_day =   day; }
@@ -11,10 +17,11 @@ int date::getMonth() { return m_month; }
 int   date::getDay() {   return m_day; }
 int  date::getYear() {  return m_year; }
 string date::str() {
-  // Create temp string
-  // Serialize month/day/year into string
-  // return temp string
-  return "";
+  string d = ""; // Create temp string
+  d += m_month;
+  d += "/" + m_day;
+  d += "/" + m_year; // Serialize date into string
+  return d;
 }
 
 // Comparison operators
@@ -24,6 +31,15 @@ bool date::operator==(const date& op) {
   //      If days are equal
   //        return true
   // else return false
+
+  if(m_year == op.m_year) {
+    if(m_month == op.m_month) {
+      if(m_day == op.m_day) {
+        return true;
+      }
+    }
+  }
+
   return false;
 }
 bool  date::operator<(const date& op) {
@@ -32,6 +48,15 @@ bool  date::operator<(const date& op) {
   //      If other's days is less than day
   //        return true
   // else return false
+
+  if(m_year <= op.m_year) {
+    if(m_month <= op.m_month) {
+      if(m_day < op.m_day) {
+        return true;
+      }
+    }
+  }
+
   return false;
 }
 
@@ -41,6 +66,14 @@ bool date::operator<=(const date& op) {
   //      If other's days is less than or equal to day
   //        return true
   // else return false
+
+  if(m_year <= op.m_year) {
+    if(m_month <= op.m_month) {
+      if(m_day <= op.m_day) {
+        return true;
+      }
+    }
+  }
   return false;
 }
 
@@ -50,6 +83,14 @@ bool  date::operator>(const date& op) {
   //      If other's days is greater than day
   //        return true
   // else return false
+
+  if(m_year >= op.m_year) {
+    if(m_month >= op.m_month) {
+      if(m_day > op.m_day) {
+        return true;
+      }
+    }
+  }
   return false;
 }
 
@@ -59,5 +100,31 @@ bool date::operator>=(const date& op) {
   //      If other's days is greater than or equal to day
   //        return true
   // else return false
+
+  if(m_year >= op.m_year) {
+    if(m_month >= op.m_month) {
+      if(m_day >= op.m_day) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+bool date::operator!=(const date& op) {
+  // If other's year is greater than or equal to year
+  //    If other's months is greater than or equal to month
+  //      If other's days is greater than or equal to day
+  //        return true
+  // else return false
+
+  if(m_year != op.m_year) {
+    if(m_month != op.m_month) {
+      if(m_day != op.m_day) {
+        return true;
+      }
+    }
+  }
+
   return false;
 }
