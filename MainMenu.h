e01a4a35cb8394a854488db149b3c365859c1c3a
bobthewalrus <chantler.james@gmail.com>
2015-03-15 19:30:27 -0700

    Create MainMenu.h

---
+++ MainMenu.h
@@ -0,0 +1,15 @@
+
+#ifndef MAINMENU_h
+#define MAINMENU_h
+
+class MainMenu
+{
+public:
+	MainMenu();
+	~MainMenu();
+	void Menu1(int);
+	void CashMenu1(int);
+	void InvMenu1(int);
+	void ReportMenu1(int);
+};
+#endif MAINMENU_h
